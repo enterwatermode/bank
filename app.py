b0a3a7d7363c92357a856fc793f30765776fc7a3
hbw <hanbinw@uci.edu>
2021-05-09 13:50:22 -0700

    add bootstrap for index, register, send, user pages

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/send")
+def send():
+    return render_template("send.html")
+
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", name=name)
